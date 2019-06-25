7e68d858d6c350641b35f98615b5936ef93a6c62
Luis Estrada <lfe9621@gmail.com>
2019-06-24 22:00:11 -0600

    actualizacion de diseño y doc de sprint 1

--- pages/inicio.php
+++ pages/inicio.php
@@ -115,12 +115,12 @@
             <li class="treeview">
               <a href="#">
                 <i class="fa fa-laptop"></i>
-                <span>Almacén</span>
+                <span>Tablero</span>
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="#"><i class="fa fa-circle-o"></i> Artículos</a></li>
-                <li><a href="#"><i class="fa fa-circle-o"></i> Categorías</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Acceso Rapido a Modulos</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Grafico de productividad</a></li>
               </ul>
             </li>
             
@@ -131,8 +131,11 @@
                  <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="#"><i class="fa fa-circle-o"></i> Ingresos</a></li>
-                <li><a href="#"><i class="fa fa-circle-o"></i> Proveedores</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i>Notificaciones del inventario</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i>Crear pedido </a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i>Modificacion de pedido </a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i>Consultar la orden de pedido </a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i>Eliminar el pedido</a></li>
               </ul>
             </li>
             <li class="treeview">
@@ -142,21 +145,55 @@
                  <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="#"><i class="fa fa-circle-o"></i> Ventas</a></li>
-                <li><a href="#"><i class="fa fa-circle-o"></i> Clientes</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Buscar medicamentos</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Facturacion</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Cotizacion</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Turno de caja</a></li>
               </ul>
             </li>
                        
             <li class="treeview">
               <a href="#">
-                <i class="fa fa-folder"></i> <span>Acceso</span>
+                <i class="fa fa-folder"></i> <span>Inventario</span>
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="#"><i class="fa fa-circle-o"></i> Usuarios</a></li>
-                
+                <li><a href="#"><i class="fa fa-circle-o"></i> Incidencias</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Productos de consumo</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Ingreso de compras</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Vencidos</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Transferencias</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Devoluciones</a></li>
               </ul>
             </li>
+
+            <li class="treeview">
+              <a href="#">
+                <i class="fa fa-folder"></i> <span>Registro</span>
+                <i class="fa fa-angle-left pull-right"></i>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="#"><i class="fa fa-circle-o"></i> SubModulo Cliente</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> SubModulo Empleado</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> SubModulo Proveedores</a></li>
+              </ul>
+            </li>
+
+            <li class="treeview">
+              <a href="#">
+                <i class="fa fa-folder"></i> <span>Reportes</span>
+                <i class="fa fa-angle-left pull-right"></i>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="#"><i class="fa fa-circle-o"></i> Reporte de inventario</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Reporte de venta</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Reporte de compra</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Reporte de empleados</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Reporte de clientes</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Reporte de proveedores</a></li>
+              </ul>
+            </li>
+
              <li>
               <a href="#">
                 <i class="fa fa-plus-square"></i> <span>Ayuda</span>
