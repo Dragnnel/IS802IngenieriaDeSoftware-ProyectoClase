7f23fa4dd26bc8dddba0068860fccc94cece1c12
Luis Estrada <lfe9621@gmail.com>
2019-06-24 21:09:51 -0600

    Actualizacion Diseño Web V2

---
+++ pages/inicio.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="es">
+  
+  <!-- Comienzo del encabezado lo mas general de sitio web -->
+  <head>
+
+    <!-- Indicar que se utilizara caracteres especiales en la pagina-->
+    <meta charset="utf-8">
+
+    <!-- Indicar que nuestro sitio web esta diseñado para ser visto desde diferentes tamaños de pantalla--> 
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Descripcion del proyecto y nombre de los autores -->
+    <meta name="description" content="Proyecto de Ingenieria de Software IS802-2019 donde se creara un Sistema Web de una Farmacia con Gestion de Sistema de Puntos.">
+    <meta name="author" content="  20151001598 Luis Estrada, 
+                                   20131004866 Jheral Blanco, 
+                                   20101600009 Bayrón Ayala,
+                                   20131015994 Nicole Ortiz, 
+                                   20141010053 Angie Argueta,
+                                   20121012100 Evert Gonzalez">
+
+    <!-- Icono o favicon de el sitio web y nombre de la pagina -->
+    <link rel="icon" href="../img/logo.png"> 
+    <title>Pagina Principal Farmacia IS802</title>
+
+    <!-- Importar las librerias de bootstrap -->
+        <!-- Bootstrap 4.3.1 -->
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
+        
+    <!--Posterior uso JS, Popper.js y jQuery -->
+        <script src="../js/jquery-3.4.1.min.js"></script>
+        <script src="../js/popper.min.js"></script>
+        <script src="../js/bootstrap.js"></script>
+        <script src="../js/app.min.js"></script>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../css/font-awesome.css">
+
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../css/AdminLTE.min.css">
+
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+         folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="../css/_all-skins.min.css">
+    <link rel="apple-touch-icon" href="../img/apple-touch-icon.png">
+    <link rel="shortcut icon" href="../img/favicon.ico">
+  </head>
+
+  <body class="hold-transition skin-blue sidebar-mini">
+    <div class="wrapper">
+
+      <header class="main-header">
+
+        <!-- Logo -->
+        <a href="#" class="logo">
+          <!-- mini logo for sidebar mini 50x50 pixels -->
+          <span class="logo-mini"><b>Farmacia IS802</b> Ing Software</span>
+          <!-- logo for regular state and mobile devices -->
+          <span class="logo-lg"><b>Farmacia</b></span>
+        </a>
+
+        <!-- Header Navbar: style can be found in header.less -->
+        <nav class="navbar navbar-static-top" role="navigation">
+          <!-- Sidebar toggle button-->
+          <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+            <span class="sr-only">Navegación</span>
+          </a>
+          <!-- Navbar Right Menu -->
+          <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+              <!-- Messages: style can be found in dropdown.less-->
+              
+              <!-- User Account: style can be found in dropdown.less -->
+              <li class="dropdown user user-menu">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                  <small class="bg-red">Online</small>
+                  <span class="hidden-xs">Nombre del usuario BD</span>
+                </a>
+                <ul class="dropdown-menu">
+                  <!-- User image -->
+                  <li class="user-header">
+                    
+                    <p>
+                      Ingenieria de Software
+                      <small>IS802</small>
+                    </p>
+                  </li>
+                  
+                  <!-- Menu Footer-->
+                  <li class="user-footer">
+                    
+                    <div class="pull-right">
+                      <a href="#" class="btn btn-default btn-flat">Cerrar</a>
+                    </div>
+                  </li>
+                </ul>
+              </li>
+              
+            </ul>
+          </div>
+
+        </nav>
+      </header>
+      <!-- Left side column. contains the logo and sidebar -->
+      <aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+          <!-- Sidebar user panel -->
+                    
+          <!-- sidebar menu: : style can be found in sidebar.less -->
+          <ul class="sidebar-menu">
+            <li class="header"></li>
+            
+            <li class="treeview">
+              <a href="#">
+                <i class="fa fa-laptop"></i>
+                <span>Almacén</span>
+                <i class="fa fa-angle-left pull-right"></i>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="#"><i class="fa fa-circle-o"></i> Artículos</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Categorías</a></li>
+              </ul>
+            </li>
+            
+            <li class="treeview">
+              <a href="#">
+                <i class="fa fa-th"></i>
+                <span>Compras</span>
+                 <i class="fa fa-angle-left pull-right"></i>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="#"><i class="fa fa-circle-o"></i> Ingresos</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Proveedores</a></li>
+              </ul>
+            </li>
+            <li class="treeview">
+              <a href="#">
+                <i class="fa fa-shopping-cart"></i>
+                <span>Ventas</span>
+                 <i class="fa fa-angle-left pull-right"></i>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="#"><i class="fa fa-circle-o"></i> Ventas</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i> Clientes</a></li>
+              </ul>
+            </li>
+                       
+            <li class="treeview">
+              <a href="#">
+                <i class="fa fa-folder"></i> <span>Acceso</span>
+                <i class="fa fa-angle-left pull-right"></i>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="#"><i class="fa fa-circle-o"></i> Usuarios</a></li>
+                
+              </ul>
+            </li>
+             <li>
+              <a href="#">
+                <i class="fa fa-plus-square"></i> <span>Ayuda</span>
+                <small class="label pull-right bg-red">PDF</small>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-info-circle"></i> <span>Acerca De...</span>
+                <small class="label pull-right bg-yellow">IT</small>
+              </a>
+            </li>
+                        
+          </ul>
+        </section>
+        <!-- /.sidebar -->
+      </aside>
+
+
+       <!--Contenido-->
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">
+        
+        <!-- Main content -->
+        <section class="content">
+          
+          <div class="row">
+            <div class="col-md-12">
+              <div class="box">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Sistema de Ventas</h3>
+                  <div class="box-tools pull-right">
+                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                    
+                    <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                  </div>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                  	<div class="row">
+	                  	<div class="col-md-12">
+		                          <!--Contenido-->
+                              <h3>Contenido</h3>
+		                          <!--Fin Contenido-->
+                           </div>
+                        </div>
+		                    
+                  		</div>
+                  	</div><!-- /.row -->
+                </div><!-- /.box-body -->
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+
+        </section><!-- /.content -->
+      </div><!-- /.content-wrapper -->
+      <!--Fin-Contenido-->
+      <footer class="main-footer">
+        <div class="pull-right hidden-xs">
+          <b>Version</b> 2.3.0
+        </div>
+        <strong>Copyright &copy; 2019 <a href="#">IS802 UNAH</a>.</strong> All rights reserved.
+      </footer>
+  </body>
+</html>
